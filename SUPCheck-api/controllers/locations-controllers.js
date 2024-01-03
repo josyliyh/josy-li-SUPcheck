cbffa11fbc3019be86b1af1da5e8a3cf2500e21f
Josy Li <josyliyh@gmail.com>
2024-01-02 22:12:57 -0800

    add weather code

--- SUPCheck-api/controllers/locations-controllers.js
+++ SUPCheck-api/controllers/locations-controllers.js
@@ -36,7 +36,8 @@ const getOneLocation = async (req, res) => {
 // Function to fetch weather data by coordinates
 const getWeatherForcast = async (latitude, longitude) => {
     try {
-      const weatherAPIURL = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,precipitation,visibility,wind_speed_10m,wind_gusts_10m&forecast_days=3&timezone=America%2FLos_Angeles`;        const response = await axios.get(weatherAPIURL);
+      const weatherAPIURL = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,precipitation,visibility,wind_speed_10m,wind_gusts_10m,weather_code&daily=sunrise,sunset&forecast_days=3&timezone=America%2FLos_Angeles`;      
+        const response = await axios.get(weatherAPIURL);
         return response.data; // Assuming the API response contains the weather data
     } catch (error) {
         console.error('Error fetching weather data:', error);
@@ -44,7 +45,7 @@ const getWeatherForcast = async (latitude, longitude) => {
     }
 };
 
-// Function to fetch temperature and rain forecast data by coordinates
+// Function to fetch marine data by coordinates
 const getMarineForcast = async (latitude, longitude) => {
   try {
       const marineAPIURL = `https://marine-api.open-meteo.com/v1/marine?latitude=${latitude}&longitude=${longitude}&hourly=wave_height&length_unit=metric&forecast_days=3&timezone=America%2FLos_Angeles`;
--- SUPcheck/src/components/WeatherInfo/WeatherInfo.jsx
+++ SUPcheck/src/components/WeatherInfo/WeatherInfo.jsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import WMOWeatherCode from "../../assets/WMO/WMO";
 
 function WeatherInfo() {
   const [weatherData, setWeatherData] = useState({});
-  const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]); // Default to current date
-  const [selectedHour, setSelectedHour] = useState('00:00');
+  const [selectedDate, setSelectedDate] = useState(
+    new Date().toISOString().split("T")[0]
+  );
   const { id } = useParams();
 
   useEffect(() => {
@@ -14,12 +16,12 @@ function WeatherInfo() {
         const response = await axios.get(`http://localhost:8080/${id}/weather`);
         if (response.status === 200) {
           setWeatherData(response.data);
-          console.log('Fetched weather data:', response.data);
+          console.log("Fetched weather data:", response.data);
         } else {
-          throw new Error('Failed to fetch data');
+          throw new Error("Failed to fetch data");
         }
       } catch (error) {
-        console.error('Error fetching location data:', error);
+        console.error("Error fetching location data:", error);
       }
     };
 
@@ -28,96 +30,132 @@ function WeatherInfo() {
 
   const handleDateSelection = (date) => {
     setSelectedDate(date);
-    console.log('Selected date:', date);
-  };
-  
-  const handleHourSelection = (hour) => {
-    setSelectedHour(hour);
-    console.log('Selected hour:', hour);
+    console.log("Selected date:", selectedDate);
   };
 
   const displayWeather = () => {
-    const selectedTime = `${selectedDate}T${selectedHour}`;
-    let selectedWeather = {};
-  
-    const weatherData1 = weatherData['weatherData1'];
-    const weatherData2 = weatherData['weatherData2'];
-  
-    if (weatherData1 && weatherData1.hourly && weatherData2 && weatherData2.hourly) {
-      const selectedWeather1 = weatherData1.hourly.time.findIndex((time) => time === selectedTime);
-      const selectedWeather2 = weatherData2.hourly.time.findIndex((time) => time === selectedTime);
-  
-      if (selectedWeather1 !== -1) {
-        selectedWeather = {
-          ...selectedWeather,
-          temperature: `${weatherData1.hourly.temperature_2m[selectedWeather1]} °C`,
-          precipitation: weatherData1.hourly.precipitation[selectedWeather1],
-          visibility: `${weatherData1.hourly.visibility[selectedWeather1]} m`,
-          wind_speed: `${weatherData1.hourly.wind_speed_10m[selectedWeather1]} kph`,
-          wind_gusts: `${weatherData1.hourly.wind_gusts_10m[selectedWeather1]} kph`,
-        };
-      }
-  
-      if (selectedWeather2 !== -1) {
-        selectedWeather = {
-          ...selectedWeather,
-          wave_height: `${weatherData2.hourly.wave_height[selectedWeather2]} m`,
-        };
-      }
-    } else {
-      return <p>Weather data not available or has an unexpected format</p>;
+    const selectedWeatherData1 = weatherData["weatherData1"];
+    const selectedWeatherData2 = weatherData["weatherData2"];
+
+    if (!selectedWeatherData1 || !selectedWeatherData2) {
+      return <p>No weather information available</p>;
     }
-  
-    if (!selectedWeather) {
-      return <p>Please select a valid time</p>;
+
+    const selectedWeatherIndex = selectedWeatherData1.hourly.time.findIndex(
+      (time) => time.split("T")[0] === selectedDate
+    );
+
+    if (selectedWeatherIndex === -1) {
+      return <p>No weather information available for the selected date</p>;
     }
-  
+
     return (
-      <div>
-        <h3>Selected Time: {selectedTime || new Date().toISOString().slice(0, 16)}</h3>
-        <ul>
-          {Object.entries(selectedWeather).map(([key, value]) => (
-            <li key={key}>
-              {key.replace(/_/g, ' ').toUpperCase()}: {value}
-            </li>
-          ))}
-        </ul>
+      <div className="weather-container">
+        <h3>Date: {selectedDate}</h3>
+        {selectedWeatherData1.hourly.time.map((time, index) => {
+          if (time.split("T")[0] === selectedDate) {
+            const waveHeight = selectedWeatherData2.hourly.wave_height[index];
+            const windSpeed = selectedWeatherData1.hourly.wind_speed_10m[index];
+            const windGust = selectedWeatherData1.hourly.wind_gusts_10m[index];
+
+            const weatherCode = selectedWeatherData1.hourly.weather_code[index];
+            const hour = parseInt(time.split('T')[1].split(':')[0]); 
+            const isDayTime = hour >= 6 && hour < 18; // Check if it's day time
+            
+            // Extract the corresponding weather description based on day/night and weather code
+            const weatherInfo = isDayTime
+              ? WMOWeatherCode[weatherCode]?.day
+              : WMOWeatherCode[weatherCode]?.night;
+            
+            // Use the weather description obtained from WMOWeatherCode
+            const weatherDescription = weatherInfo?.description || "Weather description not available";
+            
+            const weatherImage = weatherInfo?.image || "Weather image not available";
+
+
+            return (
+              <div className="weather-info" key={index}>
+                <h4>{hour}</h4>
+
+                <div className="info-item">
+                  <img src={weatherImage} alt={weatherDescription}  />
+    
+                  <span className="info-value">{weatherDescription}</span>
+                </div>
+                <div className="info-item">
+                  <span className="info-title">Temperature:</span>
+                  <span className="info-value">
+                    {selectedWeatherData1.hourly.temperature_2m[index]} °C
+                  </span>
+                </div>
+                <div className="info-item">
+                  <span className="info-title">Precipitation:</span>
+                  <span className="info-value">
+                    {selectedWeatherData1.hourly.precipitation[index]} mm
+                  </span>
+                </div>
+                <div className="info-item">
+                  <span className="info-title">Visibility:</span>
+                  <span className="info-value">
+                    {selectedWeatherData1.hourly.visibility[index]} m
+                  </span>
+                </div>
+                <div className="info-item">
+                  <span className="info-title">Wind Speed:</span>
+                  <span className="info-value">{windSpeed} km/h</span>
+                </div>
+                <div className="info-item">
+                  <span className="info-title">Wind Gust:</span>
+                  <span className="info-value">{windGust} km/h</span>
+                </div>
+                <div className="info-item">
+                  <span className="info-title">Wave Height:</span>
+                  <span className="info-value">{waveHeight} m</span>
+                </div>
+              </div>
+            );
+          }
+          return null;
+        })}
       </div>
     );
   };
-
-  const hours = Array.from({ length: 24 }, (_, i) => `0${i}`.slice(-2));
-
 
   return (
     <div>
       <h2>Weather Information for ID: {id}</h2>
       <div>
-        <button onClick={() => handleDateSelection(new Date().toISOString().split('T')[0])}>Today</button>
-        <button onClick={() => handleDateSelection(new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toISOString().split('T')[0])}>Tomorrow</button>
-        <button onClick={() => handleDateSelection(new Date(new Date().getTime() + 2 * 24 * 60 * 60 * 1000).toISOString().split('T')[0])}>Day After</button>
+        <button
+          onClick={() =>
+            handleDateSelection(new Date().toISOString().split("T")[0])
+          }
+        >
+          Today
+        </button>
+        <button
+          onClick={() =>
+            handleDateSelection(
+              new Date(new Date().getTime() + 24 * 60 * 60 * 1000)
+                .toISOString()
+                .split("T")[0]
+            )
+          }
+        >
+          Tomorrow
+        </button>
+        <button
+          onClick={() =>
+            handleDateSelection(
+              new Date(new Date().getTime() + 2 * 24 * 60 * 60 * 1000)
+                .toISOString()
+                .split("T")[0]
+            )
+          }
+        >
+          Day After
+        </button>
       </div>
-    
-      <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-        <div style={{ width: '100%', position: 'relative' }}>
-          <input
-            type="range"
-            min="0"
-            max="23"
-            step="1"
-            style={{ width: '100%' }}
-            onChange={(e) => handleHourSelection(`${String(e.target.value).padStart(2, '0')}:00`)}
-          />
-          <div style={{ display: 'flex', justifyContent: 'space-between', position: 'absolute', bottom: '-20px', width: '100%' }}>
-            {Array.from({ length: 24 }, (_, i) => (
-              <span key={i} style={{ flex: '1', textAlign: 'center' }}>
-                {i}
-              </span>
-            ))}
-          </div>
-        </div>
-      </div>
-  
+
       {displayWeather()}
     </div>
   );
