f2bd3c5a2fb291141d0bdc257d2bae4a12c7148b
Josy Li <josyliyh@gmail.com>
2024-01-07 02:37:38 -0800

    add flex remove grid

--- SUPcheck/src/components/WeatherInfo/WeatherInfo.jsx
+++ SUPcheck/src/components/WeatherInfo/WeatherInfo.jsx
@@ -224,115 +224,6 @@ function WeatherInfo() {
               {selectedWeather.temperature}
             </p>
           </li>
-          <li className="weather__data weather__data--wind">
-            <h3 className="weather__title">
-              WIND SPEED{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="14"
-                viewBox="0 0 14 14"
-              >
-                <path
-                  fill="none"
-                  stroke="#f0f0f0"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M7.5.5a1.75 1.75 0 0 1 0 3.5h-7m11.25 6.5a1.75 1.75 0 0 0 0-3.5H2m5.25 6.5a1.75 1.75 0 0 0 0-3.5H1.5"
-                />
-              </svg>
-            </h3>
-            <p className="weather__value">{selectedWeather.wind_speed}</p>
-          </li>
-          <li className="weather__data weather__data--gust">
-            <h3 className="weather__title">
-              WIND GUSTS{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="14"
-                viewBox="0 0 32 32"
-              >
-                <path
-                  fill="#f0f0f0"
-                  d="m29.316 8.051l-18-6a1 1 0 0 0-.916.149L4 7V2H2v28h2V11l6.4 4.8a1 1 0 0 0 .917.149l18-6a1 1 0 0 0 0-1.897ZM10 13L4.667 9L10 5Zm4-.054l-2 .667V4.387l2 .667Zm4-1.333l-2 .666V5.721l2 .666Zm2-.667V7.054L25.838 9Z"
-                />
-                <path
-                  fill="#f0f0f0"
-                  d="M20 22a4 4 0 0 0-8 0h2a2 2 0 1 1 2 2H8v2h8a4.005 4.005 0 0 0 4-4"
-                />
-                <path
-                  fill="#f0f0f0"
-                  d="M26 22a4.005 4.005 0 0 0-4 4h2a2 2 0 1 1 2 2H12v2h14a4 4 0 0 0 0-8"
-                />
-              </svg>
-            </h3>
-            <p className="weather__value">{selectedWeather.wind_gusts}</p>
-          </li>
-
-          <li className="weather__data weather__data--visibility">
-            <h3 className="weather__title">
-              VISIBILITY{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="14"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill="#f0f0f0"
-                  d="M12 6.5c2.76 0 5 2.24 5 5c0 .51-.1 1-.24 1.46l3.06 3.06c1.39-1.23 2.49-2.77 3.18-4.53C21.27 7.11 17 4 12 4c-1.27 0-2.49.2-3.64.57l2.17 2.17c.47-.14.96-.24 1.47-.24M2.71 3.16a.996.996 0 0 0 0 1.41l1.97 1.97A11.892 11.892 0 0 0 1 11.5C2.73 15.89 7 19 12 19c1.52 0 2.97-.3 4.31-.82l2.72 2.72a.996.996 0 1 0 1.41-1.41L4.13 3.16c-.39-.39-1.03-.39-1.42 0M12 16.5c-2.76 0-5-2.24-5-5c0-.77.18-1.5.49-2.14l1.57 1.57c-.03.18-.06.37-.06.57c0 1.66 1.34 3 3 3c.2 0 .38-.03.57-.07L14.14 16c-.65.32-1.37.5-2.14.5m2.97-5.33a2.97 2.97 0 0 0-2.64-2.64z"
-                />
-              </svg>
-            </h3>
-            <p className="weather__value">{selectedWeather.visibility}</p>
-          </li>
-          <li className="weather__data weather__data--wave">
-            <h3 className="weather__title">
-              WAVE HEIGHT{" "}
-              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"><path fill="none" stroke="#f0f0f0" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 10c2.483 0 4.345-3 4.345-3s1.862 3 4.345 3c2.482 0 4.965-3 4.965-3s2.483 3 4.345 3M3 17c2.483 0 4.345-3 4.345-3s1.862 3 4.345 3c2.482 0 4.965-3 4.965-3s2.483 3 4.345 3"/></svg>
-            </h3>
-            <p className="weather__value">{selectedWeather.wave_height}</p>
-          </li>
-
-          <li className="weather__data weather__data--precipitation">
-            <h3 className="weather__title">
-              PRECIPITATION{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="14"
-                viewBox="0 0 2048 2048"
-              >
-                <path
-                  fill="#f0f0f0"
-                  d="M1607 1166q28 57 42 118t15 124q0 88-23 170t-64 153t-100 129t-130 100t-153 65t-170 23q-88 0-170-23t-153-64t-129-100t-100-130t-65-153t-23-170q0-63 14-124t43-118L1024 0zm-583 754q106 0 199-40t163-109t110-163t40-200q0-48-11-95t-33-90l-468-937l-468 937q-22 43-33 90t-11 95q0 106 40 199t109 163t163 110t200 40"
-                />
-              </svg>
-            </h3>
-            <p className="weather__value">{selectedWeather.precipitation}</p>
-          </li>
-          <li className="weather__data weather__data--precipitation">
-            <h3 className="weather__title">
-              SAFETY{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="14"
-                height="14"
-                viewBox="0 0 1024 1024"
-              >
-                <path
-                  fill="#f0f0f0"
-                  d="M512 64L128 192v384c0 212.1 171.9 384 384 384s384-171.9 384-384V192zm312 512c0 172.3-139.7 312-312 312S200 748.3 200 576V246l312-110l312 110z"
-                />
-                <path
-                  fill="#f0f0f0"
-                  d="M378.4 475.1a35.91 35.91 0 0 0-50.9 0a35.91 35.91 0 0 0 0 50.9l129.4 129.4l2.1 2.1a33.98 33.98 0 0 0 48.1 0L730.6 434a33.98 33.98 0 0 0 0-48.1l-2.8-2.8a33.98 33.98 0 0 0-48.1 0L483 579.7z"
-                />
-              </svg>
-            </h3>
-            <p className="weather__value">{selectedWeather.safety}</p>
-          </li>
           <li className="weather__data weather__data--sun">
             <h3 className="weather__title--sun">
               SUNRISE{" "}
@@ -382,6 +273,122 @@ function WeatherInfo() {
                 .slice(0, 5)}
             </p>
           </li>
+          <div className="weather__data--wrapper">
+          <div class="row">
+            <li className="weather__data weather__data--wind">
+              <h3 className="weather__title">
+                WIND SPEED{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 14 14"
+                >
+                  <path
+                    fill="none"
+                    stroke="#f0f0f0"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M7.5.5a1.75 1.75 0 0 1 0 3.5h-7m11.25 6.5a1.75 1.75 0 0 0 0-3.5H2m5.25 6.5a1.75 1.75 0 0 0 0-3.5H1.5"
+                  />
+                </svg>
+              </h3>
+              <p className="weather__value">{selectedWeather.wind_speed}</p>
+            </li>
+            <li className="weather__data weather__data--gust">
+              <h3 className="weather__title">
+                WIND GUSTS{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 32 32"
+                >
+                  <path
+                    fill="#f0f0f0"
+                    d="m29.316 8.051l-18-6a1 1 0 0 0-.916.149L4 7V2H2v28h2V11l6.4 4.8a1 1 0 0 0 .917.149l18-6a1 1 0 0 0 0-1.897ZM10 13L4.667 9L10 5Zm4-.054l-2 .667V4.387l2 .667Zm4-1.333l-2 .666V5.721l2 .666Zm2-.667V7.054L25.838 9Z"
+                  />
+                  <path
+                    fill="#f0f0f0"
+                    d="M20 22a4 4 0 0 0-8 0h2a2 2 0 1 1 2 2H8v2h8a4.005 4.005 0 0 0 4-4"
+                  />
+                  <path
+                    fill="#f0f0f0"
+                    d="M26 22a4.005 4.005 0 0 0-4 4h2a2 2 0 1 1 2 2H12v2h14a4 4 0 0 0 0-8"
+                  />
+                </svg>
+              </h3>
+              <p className="weather__value">{selectedWeather.wind_gusts}</p>
+            </li>
+            </div>
+            <div class="row">
+            <li className="weather__data weather__data--visibility">
+              <h3 className="weather__title">
+                VISIBILITY{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#f0f0f0"
+                    d="M12 6.5c2.76 0 5 2.24 5 5c0 .51-.1 1-.24 1.46l3.06 3.06c1.39-1.23 2.49-2.77 3.18-4.53C21.27 7.11 17 4 12 4c-1.27 0-2.49.2-3.64.57l2.17 2.17c.47-.14.96-.24 1.47-.24M2.71 3.16a.996.996 0 0 0 0 1.41l1.97 1.97A11.892 11.892 0 0 0 1 11.5C2.73 15.89 7 19 12 19c1.52 0 2.97-.3 4.31-.82l2.72 2.72a.996.996 0 1 0 1.41-1.41L4.13 3.16c-.39-.39-1.03-.39-1.42 0M12 16.5c-2.76 0-5-2.24-5-5c0-.77.18-1.5.49-2.14l1.57 1.57c-.03.18-.06.37-.06.57c0 1.66 1.34 3 3 3c.2 0 .38-.03.57-.07L14.14 16c-.65.32-1.37.5-2.14.5m2.97-5.33a2.97 2.97 0 0 0-2.64-2.64z"
+                  />
+                </svg>
+              </h3>
+              <p className="weather__value">{selectedWeather.visibility}</p>
+            </li>
+            <li className="weather__data weather__data--wave">
+              <h3 className="weather__title">
+                WAVE HEIGHT{" "}
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24"><path fill="none" stroke="#f0f0f0" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 10c2.483 0 4.345-3 4.345-3s1.862 3 4.345 3c2.482 0 4.965-3 4.965-3s2.483 3 4.345 3M3 17c2.483 0 4.345-3 4.345-3s1.862 3 4.345 3c2.482 0 4.965-3 4.965-3s2.483 3 4.345 3"/></svg>
+              </h3>
+              <p className="weather__value">{selectedWeather.wave_height}</p>
+            </li>
+            </div>
+            <div class="row">
+            <li className="weather__data weather__data--precipitation">
+              <h3 className="weather__title">
+                PRECIPITATION{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 2048 2048"
+                >
+                  <path
+                    fill="#f0f0f0"
+                    d="M1607 1166q28 57 42 118t15 124q0 88-23 170t-64 153t-100 129t-130 100t-153 65t-170 23q-88 0-170-23t-153-64t-129-100t-100-130t-65-153t-23-170q0-63 14-124t43-118L1024 0zm-583 754q106 0 199-40t163-109t110-163t40-200q0-48-11-95t-33-90l-468-937l-468 937q-22 43-33 90t-11 95q0 106 40 199t109 163t163 110t200 40"
+                  />
+                </svg>
+              </h3>
+              <p className="weather__value">{selectedWeather.precipitation}</p>
+            </li>
+            <li className="weather__data weather__data--precipitation">
+              <h3 className="weather__title">
+                SAFETY{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="14"
+                  height="14"
+                  viewBox="0 0 1024 1024"
+                >
+                  <path
+                    fill="#f0f0f0"
+                    d="M512 64L128 192v384c0 212.1 171.9 384 384 384s384-171.9 384-384V192zm312 512c0 172.3-139.7 312-312 312S200 748.3 200 576V246l312-110l312 110z"
+                  />
+                  <path
+                    fill="#f0f0f0"
+                    d="M378.4 475.1a35.91 35.91 0 0 0-50.9 0a35.91 35.91 0 0 0 0 50.9l129.4 129.4l2.1 2.1a33.98 33.98 0 0 0 48.1 0L730.6 434a33.98 33.98 0 0 0 0-48.1l-2.8-2.8a33.98 33.98 0 0 0-48.1 0L483 579.7z"
+                  />
+                </svg>
+              </h3>
+              <p className="weather__value">{selectedWeather.safety}</p>
+            </li>
+            </div>
+          </div>
+
           {selectedDay !== null && <div></div>}
         </ul>
       </div>
